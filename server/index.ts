7ec565511d5f6166bd67470b176d1b873f01f588
Seu Nome <seu.email@exemplo.com>
2026-01-30 09:53:03 -0300

    fixfixfixixi

--- server/index.ts
+++ server/index.ts
@@ -2,7 +2,7 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import path from "path";
-import routes from "./routes";
+import routes from "./routes.js";
 
 const app = express();
 
